8a9260eb05a49dc5c6a12762fbcdeb78f27f0607
abhiWriteCode <abhigoswami2106@gmail.com>
2022-02-21 21:20:04 +0530

    Added

---
+++ app_old.py
@@ -0,0 +1,16 @@
+import downloading
+from client_1 import transformer_A, transformer_B
+from client_2 import transformer_C, transformer_D
+import finish
+
+
+if __name__ == '__main__':
+	downloading.download()
+
+	transformer_A.data_transform()
+	transformer_B.data_transform()
+
+	transformer_C.data_transform()
+	transformer_D.data_transform()
+
+	finish.exit()
---
+++ downloading/__init__.py
@@ -0,0 +1,1 @@
+from .downloading import download
